bf1c6f47194be25b55e821e8f58f7d757e7201e3
biwash-js <parajulibibas81@gmail.com>
2025-07-19 17:43:22 +0545

    add:password hashed using bcryptjs

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user-model";
+import { hashPassword } from "../utils/bcrypt.utils";
 
 export const register = async(req:Request,res:Response,next:NextFunction)=>{
     const{firstName,lastName,email,phone,gender,password} = req.body
 
-    const user = await User.create({
+    const user = new User({
         firstName,
         lastName,
         email,
@@ -12,7 +13,10 @@ export const register = async(req:Request,res:Response,next:NextFunction)=>{
         phone,
         gender
     })
-
+    const hashedPassword = await hashPassword(password)
+    user.password = hashedPassword
+    await user.save()
+    
     res.status(201).json({
         message:'user registered successfully',
         success:true,
