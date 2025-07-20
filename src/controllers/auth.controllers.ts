75f0168ceb09213d7d26eea0bfed3206c5e2fb44
biwash-js <parajulibibas81@gmail.com>
2025-07-20 20:20:37 +0545

    add:err handler middleware

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user-model";
 import { comparePassword, hashPassword } from "../utils/bcrypt.utils";
+import customError from "../middlewares/error-handler.middleware";
 
 export const register = async(req:Request,res:Response,next:NextFunction)=>{
+    try{
     const{firstName,lastName,email,phone,gender,password} = req.body
 
     const user = new User({
@@ -23,27 +25,32 @@ export const register = async(req:Request,res:Response,next:NextFunction)=>{
         status:'success',
         data:user
     })
+    }catch(error){
+    next(error)
+    }
 }
 
+
 export const login = async(req:Request,res:Response,next:NextFunction)=>{
+    try{
     const {email, password} = req.body
     if(!email){
-        throw new Error('email is required')
+        throw new customError('email is required',400)
     }
     if(!password){
-        throw new Error('Password is required')
+        throw new customError('Password is required',400)
     }
 
     const user:any = await User.findOne({email})
     if(!user){
-        throw new Error('credentials does not match')
+        throw new customError('credentials does not match',400)
     }
 
     const {password:userPass,...userData} = user?._doc
     const isPassMatched = await comparePassword(password,userPass)
 
     if(!isPassMatched){
-        throw new Error('credentials does not match')
+        throw new customError('credentials does not match',400)
     }
 
     res.status(201).json({
@@ -55,4 +62,7 @@ export const login = async(req:Request,res:Response,next:NextFunction)=>{
             
             }
         })
+    }catch(error){
+        next(error)
+    }
 }
---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express"
+
+class customError extends Error {
+    statusCode:number
+    status: 'success' | 'error' | 'fail'
+    success:boolean
+    isOperational:boolean
+
+    constructor(message:string,statusCode:number){
+        super(message)
+        this.isOperational = true;
+        this.statusCode = statusCode;
+        this.status = statusCode >=400 && statusCode <500 ? "fail" :"error"
+        this.success = false
+        Error.captureStackTrace(this,customError)
+    }
+}
+
+
+
+    export const errorHandler = (err:any,req: Request,res: Response,next: NextFunction) => {
+  const statusCode = err?.statusCode || 500;
+  const message =  err?.message || "Internal Server Error"
+  const success =  err?.success || false
+  const status = err?.status || "error"
+
+  res.status(statusCode).json({
+    message,
+    status,
+    success,
+    data: null
+  });
+};
+
+export default customError;
